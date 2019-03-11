972c8db3c0398595ddef88095f461c99173ae900
Ninjacop <tlutze@cannonschool.org>
2019-03-11 12:06:44 -0400

    Added a few more slots to the leaderboard

--- main.js
+++ main.js
@@ -21,9 +21,11 @@ function add(name, wins, losses, mains) {
    
 window.onload = (function() {
     add('Jake H.', 4, 0, 'Mii Swordfighter, Mii Brawler, Young/Toon Link');
+
     add('Nathan', 3, 1, 'Incineroar, Mr. G&W');
-    add('Teegan', 2, 1, 'Jigglypuff');
+
     add('Leland', 2, 1, 'Roy');
+    add('Teegan', 2, 1, 'Jigglypuff, Lucina');
     
     add('Deven', 1, 1, 'Piranha Plant');
 
@@ -32,9 +34,11 @@ window.onload = (function() {
     add('Tyler', 0, 1, 'Pikachu');
 
     add('Andrew', 0, 0, 'Meta Knight');
+    add('Ethan', 0, 0, 'Mario');
     add('Harry', 0, 0, 'Dark Pit');
     add('Jake S.', 0, 0, 'Ness, Sonic');
     add('Johnathan', 0, 0, 'R.O.B, Mr. G&W');
+    add('Mahesh', 0, 0, 'Villager');
     add('Michael', 0, 0, 'King Dedede');
     add('Vincent', 0, 0, 'King Dedede');
 });
